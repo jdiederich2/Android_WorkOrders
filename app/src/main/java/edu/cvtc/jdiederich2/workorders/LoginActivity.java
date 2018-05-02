b3f5ea482dceec1af51fe6283e993ea05255e182
jdiederich2 <jdiederich2@student.cvtc.edu>
2018-05-02 16:17:27 -0500

    Added validation for user name and password, added reset user name and password buttons

--- app/src/main/java/edu/cvtc/jdiederich2/workorders/LoginActivity.java
+++ app/src/main/java/edu/cvtc/jdiederich2/workorders/LoginActivity.java
@@ -1,9 +1,9 @@
 package edu.cvtc.jdiederich2.workorders;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,8 +17,11 @@ public class LoginActivity extends AppCompatActivity {
     private EditText userName;
     private EditText password;
     private TextView numOfLogAttempts;
+    private TextView loginError;
     private Button login;
     private int counter = 5;
+    private Button resetUserName;
+    private Button resetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +31,10 @@ public class LoginActivity extends AppCompatActivity {
         userName = (EditText) findViewById(R.id.editText_UserName);
         password = (EditText) findViewById(R.id.editText_Password);
         numOfLogAttempts = (TextView) findViewById(R.id.textView_LoginAttempts);
+        loginError = (TextView) findViewById(R.id.textView_loginError);
         login = (Button) findViewById(R.id.button_Login);
+        resetUserName = (Button) findViewById(R.id.button_forgotUserName);
+        resetPassword = (Button) findViewById(R.id.button_forgotPassword);
 
         numOfLogAttempts.setText("Number of attempts remaining: 5");
 
@@ -38,24 +44,45 @@ public class LoginActivity extends AppCompatActivity {
                 validateUser(userName.getText().toString(), password.getText().toString());
             }
         });
+
+        resetUserName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ResetUserNameActivity.this);
+                startActivity(intent);
+            }
+        });
+
+        resetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ResetPasswordActivity.this);
+                startActivity(intent);
+            }
+        });
     }
 
     private void validateUser(String userName, String password) {
         String userNameTest = "jdiederich";
         String passwordTest = "1234";
 
-        if ((userName == userNameTest) || (password == passwordTest)) {
+        if (userName.equals(userNameTest) || password.equals(passwordTest)) {
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
-            Log.d(  "1","UserValidated" );
+
         } else {
             counter --;
-            Log.d(  "1","UserNotValidated" );
+            loginError.setVisibility(View.VISIBLE);
+            loginError.setTextColor(Color.CYAN);
+            loginError.setText("Incorrect user name or password");
+
+            this.userName.setText("");
+            this.password.setText("");
+
             numOfLogAttempts.setText("No of attempts remaining: " + valueOf(counter));
 
             if(counter == 0) {
                 login.setEnabled(false);
-
 
             }
         }
